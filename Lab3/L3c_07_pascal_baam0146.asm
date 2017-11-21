74d7cb9228dc6ed509a98d425f76dde890659e6d
balazsadama <balazs.adam.a@gmail.com>
2017-11-21 17:54:05 +0200

    Messing around, doesn't work yet

--- Lab3/L3c_07_pascal_baam0146.asm
+++ Lab3/L3c_07_pascal_baam0146.asm
@@ -14,18 +14,58 @@ global main
 section .text
 main:
 
-	mov		eax, str_szabaly
-	call	mio_writestr
+	mov		edi, str_res
+	mov		eax, [str_res_len]
+	stosb
+	mov		esi, str_res
+	call	write_str_pascal
 	call	mio_writeln
 	
-	mov		eax, str_be
-	call	mio_writestr
+	mov		edi, str_res
+	add		edi, 6
+	xor		eax, eax
+	mov		al, 'e'
+	stosb
+	mov		al, 'd'
+	stosb
+	mov		al, 'c'
+	stosb
+	mov		al, 'b'
+	stosb
+	mov		al, 'a'
+	stosb
 	
-	mov		edi, str_A
-	call	read_str_pascal2
+	mov		edi, str_res
+	;add		byte [str_res_len], 5	; noveljuk az eredmeny karakterlanc hosszat
+	;mov		eax, [str_res_len]
+	mov		eax, 11
+	stosb
 	
-	mov		edi, str_A
 	call	write_str_pascal
+	call	mio_writeln
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	call	read_input
+	call	write_given_input
+	
+	call	solve
+	call	write_res
 	
 	ret
 
@@ -39,8 +79,10 @@ read_str_pascal:
 	push	ebx
 	push	ecx
 	push	edx
+	push	edi
 	
 	mov		ebx, edi		; elmentjuk a karakterlanc memoriabeli kezdocimet, hogy be tudjuk irni a hosszat
+	inc		edi
 	xor		ecx, ecx		; szamoljuk a beolvasott karaktereket
 	
 .loop_read:
@@ -50,17 +92,19 @@ read_str_pascal:
 	
 	cmp		al, 13			; ha lenyomjak az 'enter' billentyut
 	je		.stop_read
-	mov		[esi], al
-	inc		esi
+	stosb
 	inc		ecx				; karakterek szamat noveljuk
 	jmp		.loop_read
 	
 	
 .stop_read:
-	mov		[ebx], ecx		; a 0. karaktert beallitjuk a hosszra
-	mov		eax, 10			; uj sor
-	call	mio_writechar
+	mov		eax, ecx		; a 0. karaktert beallitjuk a hosszra
+	mov		edi, ebx		; az elso poziciora irjuk a hosszt
+	stosb
 	
+	call	mio_writeln
+	
+	pop		edi
 	pop		edx
 	pop		ecx
 	pop		ebx
@@ -73,27 +117,233 @@ write_str_pascal:
 	push	ebx
 	push	ecx
 	push	edx
+	push	esi
 	
-	mov		ecx, [esi]		; a hosszt berakjuk ECX-be (loop)
-	inc		esi
-.loop_write:
 	xor		eax, eax
-	mov		eax, [esi]
+	lodsb					; betoltjuk a hosszt
+	mov		ecx, eax		; beallitjuk a loop counter-t
+	jecxz	.end
+.loop_write:
+	lodsb
 	call	mio_writechar
-	inc		esi
 	loop	.loop_write
 	
+.end:
+	call	mio_writeln
+	pop		esi
 	pop		edx
 	pop		ecx
 	pop		ebx
 	pop		eax
 	ret
 	
+	
+solve:
+	push	eax				; elmentjuk az eredeti ertekeket
+	push	ebx
+	push	ecx
+	push	edx
+	push	esi
+	push	edi
+	
+	
+	mov		edi, str_res
+	add		edi, 6			; mert "0abcde" a str_res kezdeti erteke
 
+	
+	mov		esi, str_A		; beallitjuk a forras cimet
+	
+	xor		eax, eax		; a feldolgozando karakterlanc hosszat az ECX-be tesszuk
+	lodsb
+	mov		ecx, eax
+	
+	mov		ebx, 5			; eredmeny karakterlanc hossza
+	xor		edx, edx		; szamoljuk az idezojelek kozti karaktereket
+	
+.loop_quote:
+	jecxz	.end_A
+	lodsb
+	
+	cmp		al, '"'
+	je		.is_quote
+	
+	cmp		ebx, 0
+	jne		.count_chars
+	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;loop	.loop_quote		; ha nincs idezojel nyitva, akkor vesszuk a kov. karaktert
+	dec		ecx
+	jmp		.loop_quote
+	
+.count_chars:
+	inc		edx				; kulonben noveljuk az idezojelek kozti karakterek szamat
+	dec		ecx
+	jmp		.loop_quote
+	
+.is_quote:
+	push	ebx				; quotes = 0 ha nincs zarojel nyitva, kulonben 1
+	mov		ebx, [quotes]
+	cmp		ebx, 0
+	pop		ebx
+	je		.start_quote	; ha 0 akkor idezojel nyitva
+	
+	; ha idezojel zarva, akkor kiirjuk a koztuk levo karaktereket
+	mov		[qotes], 0		; jelezzuk, hogy vege az idezojelnek
+	
+	
+
+	
+	;jecxz	.loop_quote		; ha nincs karakter idezojelek kozott, akkor vizsgaljuk a kovetkezo karaktert
+	cmp		edx, 0
+	loop	.loop_quote
+	
+	
+	
+	
+	
+	
+	
+	sub		esi, edx		; visszalepunk a nyitott idezojel utani karakterre
+	sub		esi, 1
+	
+	push	ecx
+	mov		ecx, edx
+.between_quotes:
+	lodsb
+	stosb
+	;call	mio_writechar		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
+	inc		ebx				; noveljuk az eredmeny karakterlanc hosszat
+	dec		ecx
+	jmp		.loop_quote
+	
+	inc		esi				; lep a zaro idezojel utan kovetkezo beture
+	pop		ecx				; helyreallitjuk ecx erteket
+	xor		edx, edx		; az idezojelek kozti karakterek szamat lenullazzuk, hiszen kiirtuk oket
+	dec		ecx
+	jmp		.loop_quote
+	
+.start_quote:
+	mov		[qotes], 1
+	dec		ecx
+	jmp		.loop_quote
+	
+.end_A:
+	mov		al, 'e'
+	stosb
+	mov		al, 'd'
+	stosb
+	mov		al, 'c'
+	stosb
+	mov		al, 'b'
+	stosb
+	mov		al, 'a'
+	stosb
+	add		ebx, 5	; noveljuk az eredmeny karakterlanc hosszat
+
+	; feldolgozzuk a B karakterlancot
+	mov		esi, str_B
+	xor		eax, eax		; a karakterlanc hosszat az ECX-be tesszuk
+	lodsb
+	mov		ecx, eax
+.loop_B:
+	jecxz	.end
+	lodsb
+	
+	cmp		al, 'a'
+	jb		.add_to_res
+	
+	cmp		al, 'z'
+	jae		.add_to_res
+	
+	inc		al				; rakovetkezo beture helyettesitjuk
+.add_to_res:
+	stosb
+	inc		ebx				; noveljuk az eredmeny karakterlanc hosszat
+	dec		ecx
+	dec		ecx
+	jmp		.loop_quote
+	
+.end:
+	mov		edi, str_res
+	mov		eax, ebx
+	stosb
+	;call	mio_writeln
+	pop		edi
+	pop		esi
+	pop		edx
+	pop		ecx
+	pop		ebx
+	pop		eax
+	ret
+	
+	
+	
+	
+	
+	
+read_input:
+	push	eax
+	push	edi
+	
+	mov		eax, str_be
+	call	mio_writestr
+	
+	mov		edi, str_A
+	call	read_str_pascal
+
+	mov		eax, str_be
+	call	mio_writestr
+	
+	mov		edi, str_B
+	call	read_str_pascal
+	
+	pop		edi
+	pop		eax
+	
+	ret
+	
+	
+write_given_input:
+	push	eax
+	push	esi
+
+	mov		eax, str_szabaly
+	call	mio_writestr
+	call	mio_writeln
+	
+	mov		eax, 'A'
+	call	mio_writechar
+	mov		eax, '='
+	call	mio_writechar
+	mov		esi, str_A
+	call	write_str_pascal
+	
+	mov		eax, 'B'
+	call	mio_writechar
+	mov		eax, '='
+	call	mio_writechar
+	mov		esi, str_B
+	call	write_str_pascal
+	
+	pop		esi
+	pop		eax
+	ret
+	
+write_res:
+	push	esi
+	
+	mov		esi, str_res
+	call	write_str_pascal
+	
+	pop		esi
+	ret
+	
+	
 section .data
 	str_be			db	'Adjon meg egy karakterlancot: ', 0
 	str_szabaly		db	'"abcde" + [A-bol azok a karakterek, amelyek "" jelek kozott vannak (minden masodik " jel utan az a szakasz befejezodik es egy ujabb " kell ahhoz, hogy ujabb szakasz nyiljon, valamint be is kell fejezodjon "-al, hogy ervenyes legyen)] + "edcba" + [B, minden kisbetut a rakovetkezo betuvel helyettesitjuk (kiveve a z-t, az marad)]', 0
-
+	str_res			db	'0abcde'	; az elso karakter folul lesz irva a hosszal
+	;str_res_len		db	5			; az eredmeny karakterlanc hossza
+	quotes			db	0
 section .bss
 	str_A		resb	256
 	str_B		resb	256
