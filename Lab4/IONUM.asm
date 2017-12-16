cc0992ab5c5efcf8c3fcef08aec4cb4338cd2f2a
balazsadama <balazs.adam.a@gmail.com>
2017-12-16 14:25:14 +0200

    A 32 bites fuggvenyek megvannak, talan jol is mukodnek

--- Lab4/IONUM.asm
+++ Lab4/IONUM.asm
@@ -36,17 +36,44 @@ global WriteHex64
 section .text
 main:
 
-	clc
-	call	ReadInt
+ .repeat_ReadInt:
+	; clc
+	; call	ReadInt
+	; jc		.hiba_ReadInt
 	
-	jc		.dunno
-	mov		ecx, 0x696969
+	; call	mio_writeln
+	; call	WriteInt
 	
-.dunno:
-	mov		ebx, 0x12321
-
+ .repeat_ReadBin:
+	; call	mio_writeln
+	; clc
+	; call	ReadBin
+	; jc		.hiba_ReadBin
+	
+	; call	mio_writeln
+	; call	WriteBin
+	
+.repeat_ReadHex:
+	call	ReadHex
+	jc		.hiba_ReadHex
+	
+	call	mio_writeln
+	call	WriteHex
+	
 	ret
 	
+.hiba_ReadInt:
+	mov		eax, 0x69
+	jmp		.repeat_ReadInt
+
+.hiba_ReadBin:
+	mov		eax, 0x70
+	jmp		.repeat_ReadBin
+	
+.hiba_ReadHex:
+	call	mio_writeln
+	mov		eax, 0x71
+	jmp		.repeat_ReadBin
 	
 	
 ReadInt:
@@ -103,7 +130,7 @@ ReadInt:
 	
 	cmp		bl, '-'
 	jne		.skipNegative
-	mov		BYTE [esp], bl; hogy a vegen tudjuk, hogy kell-e negalni a szamot
+	mov		BYTE [esp], bl		; hogy a vegen tudjuk, hogy kell-e negalni a szamot
 	inc		edx
 	jmp		.loop_build
 	
@@ -154,3 +181,335 @@ ReadInt:
 	pop		ebx
 	
 	ret
+	
+	
+	
+	
+	
+WriteInt:
+	push	eax				; elmentjuk az eredeti ertekeket
+	push	ebx
+	push	ecx
+	push	edx
+
+	push	-1				; megallasi feltetel
+	cmp		eax, 0
+	jge		to_stack		; ha pozitiv, nem irjuk ki a '-' jelet
+	push	eax
+	mov		eax, '-'
+	call	mio_writechar
+	pop		eax
+	neg		eax
+	
+to_stack:
+	xor		edx, edx			; EDX-et nullara allitjuk, hogy jol osszon
+	mov		ebx, 10				; tizzel osztunk, hogy az utolso szamjegyet kapjuk maradekkent
+	div		ebx	
+	add		edx, 48				; karakterre alakitjuk szamjegybol
+	push	edx
+	cmp		eax, 0
+	je		from_stack
+	jmp		to_stack
+	
+from_stack:
+	pop		eax
+	cmp		eax, -1
+	je		stop
+	call	mio_writechar
+	jmp		from_stack
+	
+stop:							; helyreallitja az eredeti ertekeket
+	mov		eax, 10
+	call	mio_writechar		; uj sor
+	pop		edx
+	pop		ecx
+	pop		ebx
+	pop		eax
+	ret
+
+	
+	
+	
+	
+ReadBin:
+	push	ebx
+	push	ecx
+	push	edx
+	
+	xor		eax, eax			; itt epitjuk a szamot
+	xor		ecx, ecx			; szamoljuk, hogy hany karakter volt eddig beolvasva
+	
+	push	ebp					; a veremben "lokalisan memoriat foglalunk"
+    mov		ebp, esp
+	sub		esp, 32				; max 32 karaktert (0/1) adhat meg a felhasznalo
+	
+.loop_read:
+	call	mio_readchar
+	cmp		al, 13				; ha enter
+	je		.end_read
+	call	mio_writechar
+	inc		ecx					; noveljuk a beolvasott karakterek szamat
+	
+	cmp		al, 0x08			; ha backspace
+	je		.backspace
+	
+	cmp		ecx, 32
+	jg		.loop_read			; ha mar tobb mint 32 karaktert olvastunk be, akkor azt nem taroljuk
+	
+	mov		BYTE [esp + ecx - 1], al
+	jmp		.loop_read
+	
+.backspace:
+	jecxz	.loop_read			; ha nincs karakter a kepernyon, akkor a backspace-nek nincs hatasa
+	
+	mov		al, 0x20			; space
+	call	mio_writechar
+	mov		al, 0x08			; backspace
+	call	mio_writechar
+	dec		ecx					; backspace miatt
+	dec		ecx					; kitorolt karakter miatt
+	jmp		.loop_read
+	
+	
+.end_read:
+	xor		ebx, ebx			; hogy kesobb osszeadasnal ne legyen szemet ertek benne
+	xor		edx, edx			; szamoljuk a feldolgozott karaktereket
+	xor		eax, eax			; epitjuk a szamot
+	
+	cmp		ecx, 32
+	jg		.error				; ha tul hosszu a szam, akkor hiba
+	
+.loop_build:
+	cmp		edx, ecx
+	je		.end
+	
+	mov		BYTE bl, [esp + edx]; betoltjuk az elso karaktert
+	cmp		bl, '0'
+	jb		.error
+	
+	cmp		bl, '1'
+	ja		.error
+	
+	sub		bl, '0'
+	
+	shl		eax, 1				; balra toljuk egy bittel
+	or		eax, ebx			; hozzaadjuk a bitet
+	
+	inc		edx
+	jmp		.loop_build
+	
+.error:
+	stc
+	jmp		.pop_return
+	
+.end:
+	clc
+	
+.pop_return:
+	mov		esp, ebp
+    pop		ebp
+	
+	pop		edx
+	pop		ecx
+	pop		ebx
+	
+	ret
+	
+	
+	
+	
+	
+	
+	
+WriteBin:
+	push	eax
+	push	ebx
+	push	ecx
+	push	edx
+	
+	mov		ebx, eax			; eax-et hasznaljuk kiirasnal
+	mov		ecx, 32				; beallitjuk loop counter-t
+
+.bin_loop:
+	shl		ebx, 1				; eltoljuk a bitet a Carry-be
+	jc		.digit_one
+	mov		al, '0'
+	jmp		.write_digit
+	
+.digit_one:
+	mov		al, '1'
+	
+.write_digit:
+	call	mio_writechar
+	
+	; ellenorzi, hogy kell-e szokozt irjon
+	mov		eax, ecx			; osztandot eax-be tesszuk
+	add		eax, 3
+	cdq
+	push	ebx					; elmetnjuk az eredeti erteket, hogy osszunk 4-gyel
+	mov		ebx, 4
+	idiv	ebx
+	pop		ebx					; helyreallitjuk az eredeti erteket
+	
+	cmp		edx, 0				; ha oszthato 4-gyel, akkor kiirunk egy szokozt
+	jne		.loop_write
+	mov		al, ' '
+	call	mio_writechar
+	
+.loop_write:
+	loop	.bin_loop
+	
+	pop		edx
+	pop		ecx
+	pop		ebx
+	pop		eax
+	ret
+	
+	
+	
+ReadHex:
+	push	ebx
+	push	ecx
+	push	edx
+	
+	xor		eax, eax			; itt epitjuk a szamot
+	xor		ecx, ecx			; szamoljuk, hogy hany karakter volt eddig beolvasva
+	
+	push	ebp					; a veremben "lokalisan memoriat foglalunk"
+    mov		ebp, esp
+	sub		esp, 8				; max 8 karaktert adhat meg a felhasznalo
+	
+.loop_read:
+	call	mio_readchar
+	cmp		al, 13				; ha enter
+	je		.end_read
+	call	mio_writechar
+	inc		ecx					; noveljuk a beolvasott karakterek szamat
+	
+	cmp		al, 0x08			; ha backspace
+	je		.backspace
+	
+	cmp		ecx, 8
+	jg		.loop_read			; ha mar tobb mint 8 karaktert olvastunk be, akkor azt nem taroljuk
+	
+	mov		BYTE [esp + ecx - 1], al
+	jmp		.loop_read
+	
+.backspace:
+	jecxz	.loop_read			; ha nincs karakter a kepernyon, akkor a backspace-nek nincs hatasa
+	
+	mov		al, 0x20			; space
+	call	mio_writechar
+	mov		al, 0x08			; backspace
+	call	mio_writechar
+	dec		ecx					; backspace miatt
+	dec		ecx					; kitorolt karakter miatt
+	jmp		.loop_read
+	
+	
+.end_read:
+	xor		ebx, ebx			; hogy kesobb osszeadasnal ne legyen szemet ertek benne
+	xor		edx, edx			; szamoljuk a feldolgozott karaktereket
+	xor		eax, eax			; epitjuk a szamot
+	
+	cmp		ecx, 8
+	jg		.error				; ha tul hosszu a szam, akkor hiba
+	
+.loop_build:
+	cmp		edx, ecx
+	je		.end
+	
+	mov		BYTE bl, [esp + edx]; betoltjuk az elso karaktert
+	cmp		bl, '0'
+	jb		.error
+	
+	cmp		bl, '9'
+	ja		.check_uppercase
+	
+	sub		bl, '0'				; ha 0-9 kozti karakter
+	jmp		.good_value
+	
+.check_uppercase:
+	cmp		bl, 'A'
+	jl		.error
+	cmp		bl, 'F'
+	jg		.check_lowercase
+	
+	sub		bl, 'A'
+	add		bl, 10
+	jmp		.good_value
+	
+.check_lowercase:
+	cmp		bl, 'a'
+	jl		.error
+	cmp		bl, 'f'
+	jg		.error
+	
+	sub		bl, 'a'
+	add		bl, 10
+	jmp		.good_value
+	
+	
+.good_value:
+	shl		eax, 4				; balra tolunk, hogy az uj szamjegyet hozzaragasszuk
+	add		eax, ebx
+
+	inc		edx
+	jmp		.loop_build
+	
+	
+.error:
+	stc
+	jmp		.pop_return
+	
+.end:
+	clc
+	
+.pop_return:
+	mov		esp, ebp
+    pop		ebp
+	
+	pop		edx
+	pop		ecx
+	pop		ebx
+	
+	ret	
+	
+	
+WriteHex:
+	push	eax				; elmentjuk az eredeti ertekeket
+	push	ebx
+	push	ecx
+	push	edx
+	
+	mov		ebx, eax		; eredeti bemenetet EBX-ben taroljuk
+	mov		ecx, 8			; loop count
+	
+.loop_write:
+	rol		ebx, 4			; az elso szot a vegere forgatjuk
+	mov		eax, ebx
+	and		eax, 1111b		; csak utolso szot tartjuk meg
+	cmp		eax, 9
+	jg		.hex_letter
+	add		eax, '0'		; ha numerikus, kiirjuk
+	call	mio_writechar
+	jmp		.write_hex_iterate
+
+.hex_letter:
+	sub		eax, 10			; atalakitjuk A-F alakba ha a szam nagyobb tiznel
+	add		eax, 'A'
+	call	mio_writechar
+	
+.write_hex_iterate:
+	loop	.loop_write
+
+	mov		eax, 10
+	call	mio_writechar
+	pop		edx
+	pop		ecx
+	pop		ebx
+	pop		eax
+	ret
+	
+	
+	
